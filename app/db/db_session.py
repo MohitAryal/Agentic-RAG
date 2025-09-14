9835b84a7d04a0e239d08e8e9c8dee0fc7d14d1d
MohitAryal <cozahitman@gmail.com>
2025-09-14 17:44:04 +0545

    Added code for establishin connection to db

---
+++ app/db/db_session.py
@@ -0,0 +1,21 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Create async engine
+engine = create_async_engine(DATABASE_URL, echo=True, future=True)
+
+# Create session factory
+async_session = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
+
+# Dependency for FastAPI
+async def get_session() -> AsyncSession:
+    async with async_session() as session:
+        yield session
