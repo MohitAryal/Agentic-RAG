79b496b7528bea45d4b46abfec7a9893de019b23
MohitAryal <cozahitman@gmail.com>
2025-09-14 17:54:56 +0545

    imported env var using os

--- app/services/embedding.py
+++ app/services/embedding.py
@@ -1,10 +1,12 @@
 from sentence_transformers import SentenceTransformer
 import asyncio
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
+embedding_model = os.getenv("EMBEDDING_MODEL")
 
-model = SentenceTransformer(EMBEDDING_MODEL)
+model = SentenceTransformer(embedding_model)
 
 async def generate_embeddings(chunks: list[str]) -> list[list[float]]:
     loop = asyncio.get_running_loop()
