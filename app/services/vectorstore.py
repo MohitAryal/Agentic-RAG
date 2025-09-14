a2fe51bb9a4d3debf7c8483e19606c7ea0b26baa
MohitAryal <cozahitman@gmail.com>
2025-09-14 18:00:10 +0545

    imported env var using os

--- app/services/vectorstore.py
+++ app/services/vectorstore.py
@@ -3,10 +3,12 @@ from qdrant_client.http.models import PointStruct
 import uuid
 import asyncio
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
+qdrant_url = os.getenv(QDRANT_URL)
 
-qdrant = QdrantClient(url=QDRANT_URL)
+qdrant = QdrantClient(url=qdrant_url)
 
 async def save_embeddings_to_vector_db(chunks: list[str], embeddings: list[list[float]], metadata: dict):
     points = []
