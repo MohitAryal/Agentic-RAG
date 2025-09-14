ed8bd6ffcf9659b7ec42a73d56be83c96acaeb7d
MohitAryal <cozahitman@gmail.com>
2025-09-14 13:37:04 +0545

    Added code for saving metadata

---
+++ app/services/save_metadata.py
@@ -0,0 +1,10 @@
+from app.db.db_session import async_session
+from app.db.sqlmodels import FileMetadata
+import asyncio
+
+async def save_file_metadata(filename: str, chunking_method: str, chunk_count: int, embedding_model: str):
+    async with async_session() as session:
+        async with session.begin():
+            file_meta = FileMetadata(filename=filename, chunking_method=chunking_method, chunks=chunk_count, used_emb_model=embedding_model)
+            session.add(file_meta)
+        await session.commit()
